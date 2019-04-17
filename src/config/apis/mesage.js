0fb27c4ddd789406b65406789a1063ca2708e8ac
trunghv <trunghv@vinsofts.net>
2019-04-17 17:52:16 +0700

    add laravel echo && ve UI chat

--- src/config/apis/mesage.js
+++ src/config/apis/mesage.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 import constant from './constant';
 import { getItem } from 'config/Controller';
+import Echo from 'laravel-echo/dist/echo';
+import SocketIOClient from 'socket.io-client';
 let instance = axios.create({
     baseURL:constant.BASEURL,
     timeout: constant.SERVER_TIMEOUT,
@@ -10,11 +12,11 @@ let instance = axios.create({
     }
 });
 
-export const getMessage=async(page= '1')=>{
+export const getMessage=async()=>{
  
-//     let token = await getItem('token')
-      //   instance.defaults.headers.common['Authorization'] = "Bearer " + token;
-        return instance.get(constant.MESSAGE,{params:{page:page}})
+    let token = await getItem('token')
+        instance.defaults.headers.common['Authorization'] = "Bearer " + token;
+        return instance.get(constant.MESSAGE)
 }
 export const postMessage=async(body)=>{
  
@@ -22,3 +24,23 @@ export const postMessage=async(body)=>{
         instance.defaults.headers.common['Authorization'] = "Bearer " + token;
         return instance.post(constant.MESSAGE,body)
 }
+
+export const LaravelEchoConfig = async () => {
+    let token =await getItem('token')
+    let echo = new Echo({
+      broadcaster: 'socket.io',
+      host: constant.BASE_SOCKET,
+      client: SocketIOClient,
+      auth: {
+        headers: {
+          'Authorization': 'Bearer '  + token
+        },
+      },
+    });
+
+      
+      echo.channel('chatroom')
+      .listen('MessagePosted', data => {
+          return data
+      })
+  }
